b56819d6308996acf6e65a199b5fb9bc50ac7a6b
cpt62 <rgrturzn7316@gmail.com>
2026-09-05 10:27:16 +0300

    entity CatsGrade refactored

--- src/main/java/entity/CatsGrade.java
+++ src/main/java/entity/CatsGrade.java
@@ -1,4 +1,4 @@
-package Entity;
+package entity;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
@@ -7,14 +7,14 @@ public class CatsGrade {
     private final String text;
     private final String type;
     private final String user;
-    private final int upvotes;
+    private final Integer upvotes;
 
     public CatsGrade(
             @JsonProperty("id") String id
             , @JsonProperty("text") String text
             , @JsonProperty("type") String type
             , @JsonProperty("user") String user
-            , @JsonProperty("upvotes") int upvotes) {
+            , @JsonProperty("upvotes") Integer upvotes) {
         this.id = id;
         this.text = text;
         this.type = type;
@@ -38,18 +38,18 @@ public class CatsGrade {
         return user;
     }
 
-    public int getUpvotes() {
+    public Integer getUpvotes() {
         return upvotes;
     }
 
     @Override
     public String toString() {
-        return "CatsGrade{" +
-                "id='" + id + '\'' +
-                ", text='" + text + '\'' +
-                ", type='" + type + '\'' +
-                ", user='" + user + '\'' +
-                ", upvotes=" + upvotes +
+        return "CatsGrade: " +
+                "id= '" + id + '\'' +
+                ", text= '" + text + '\'' +
+                ", type= '" + type + '\'' +
+                ", user= '" + user + '\'' +
+                ", upvotes= " + upvotes +
                 '}';
     }
 }
---
+++ src/main/java/service/HttpClientCustom.java
@@ -0,0 +1,62 @@
+package Service;
+
+import Entity.CatsGrade;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.time.Duration;
+import java.util.Comparator;
+import java.util.List;
+
+public class HttpClientCustom {
+    private static final String CATS_URL =
+            "https://raw.githubusercontent.com/netology-code/jd-homeworks/master/http/task1/cats";
+
+    public static void main(String[] args) {
+        ObjectMapper mapper = new ObjectMapper();
+
+        HttpClient client = getHttpClient();
+        try {
+            HttpRequest request = getHttpRequest();
+            HttpResponse<InputStream> response = client.send(request, HttpResponse.BodyHandlers.ofInputStream());
+            if (response.statusCode() == 200) {
+                try (InputStream jsonResponse = response.body()) {
+                    List<CatsGrade> catsGradeList = mapper.readValue(jsonResponse, new TypeReference<List<CatsGrade>>() {
+                    });
+                    catsGradeList.stream()
+                            .filter(c -> c.getUpvotes() > 0)
+                            .sorted(Comparator.comparingInt(CatsGrade::getUpvotes))
+                            .forEach(System.out::println);
+                }
+            } else {
+                System.err.println(response.statusCode());
+            }
+        } catch (IllegalArgumentException e) {
+            System.out.println("Ошибка аргумента (URL)");
+        } catch (IOException ioException) {
+            System.err.println(ioException.getMessage());
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static HttpClient getHttpClient() {
+        return HttpClient.newBuilder()
+                .connectTimeout(Duration.ofSeconds(3))
+                .build();
+    }
+
+    private static HttpRequest getHttpRequest() throws IllegalArgumentException {
+        return HttpRequest.newBuilder(URI.create(CATS_URL))
+                .GET()
+                .setHeader("Accept", "application/json")
+                .timeout(Duration.ofSeconds(30))
+                .build();
+    }
+}
